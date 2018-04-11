b389f51c659770e07720e533e3ba1b01e8a6cc0b
Claus Tøndering <claus@ezer.dk>
2018-04-11 09:37:32 +0200

    Added ambiguous verb hinting

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Copyright © 2017 Ezer IT Consulting.
+# Copyright © 2018 Ezer IT Consulting.
 # Released under an MIT License.
 
 OBJFILES=emdros_iterators.o emdros_updater.o frequency_handler.o \
@@ -6,19 +6,22 @@ verb_stem_handler.o verb_class.o verb_class_handler.o strip_handler.o \
 translit_handler.o suffix_handler.o hebrew_transliterator.o util.o
 
 OBJFILES2=worddb.o emdros_iterators.o
+OBJFILES3=hintsdb.o emdros_iterators.o csv.o
 
-DEPFILES=$(OBJFILES:.o=.d) $(OBJFILES2:.o=.d)
+DEPFILES=$(OBJFILES:.o=.d) $(OBJFILES2:.o=.d) $(OBJFILES3:.o=.d)
 
-CXXFLAGS=-std=c++11 -MMD -O3 -I /usr/local/include
+CXX=7-2-0-g++
+
+CXXFLAGS=-std=c++17 -MMD -O3 -I /usr/local/include
 LDLIBS= -L /usr/local/lib 
 LDFLAGS= -lpcrecpp -lmql -lemdf
 
 BHS4=../bhs4/bhs4  # Location of source Emdros database
 
-all: ETCBC4 ETCBC4_words.db
+all: ETCBC4 ETCBC4_words.db ETCBC4_hints.db
 
 emdros_updater:	$(OBJFILES)
-	g++ $(CXXFLAGS) $(LDLIBS) -o $@ $+ $(LDFLAGS)
+	$(CXX) $(CXXFLAGS) $(LDLIBS) -o $@ $+ $(LDFLAGS)
 
 update.mql:	emdros_updater $(BHS4)
 	./emdros_updater $(BHS4) $@
@@ -34,7 +37,7 @@ ETCBC4:	update.mql
 	cd extra_tenses; ./make_update.sh; mv ETCBC4 ..
 
 worddb: $(OBJFILES2)
-	g++ $(CXXFLAGS) $(LDLIBS) -o $@ $+ $(LDFLAGS)
+	$(CXX) $(CXXFLAGS) $(LDLIBS) -o $@ $+ $(LDFLAGS)
 
 worddb.sql: worddb ETCBC4
 	./worddb ETCBC4 $@
@@ -43,8 +46,23 @@ ETCBC4_words.db: worddb.sql
 	rm -f $@
 	sqlite3 $@ < $+
 
+csv.o:	csv-parser-master/csv.cc
+	$(CXX) $(CXXFLAGS) -c $+
+
+
+hintsdb: $(OBJFILES3)
+	$(CXX) $(CXXFLAGS) $(LDLIBS) -o $@ $+ $(LDFLAGS)
+
+hintsdb.sql: hintsdb ETCBC4
+	./hintsdb ETCBC4 $@
+
+ETCBC4_hints.db: hintsdb.sql
+	rm -f $@
+	sqlite3 $@ < $+
+
+
 clean:
-	rm -f $(OBJFILES) $(OBJFILES2) $(DEPFILES) emdros_updater update.mql ETCBC4 ETCBC4_words.db worddb.sql worddb
+	rm -f $(OBJFILES) $(OBJFILES2) $(OBJFILES3) $(DEPFILES) emdros_updater update.mql ETCBC4 ETCBC4_words.db ETCBC4_hints.db hintsdb.sql hintsdb worddb.sql worddb
 
 
 -include $(DEPFILES)
---
+++ hintsdb.cpp
@@ -0,0 +1,271 @@
+/* Copyright © 2018 Ezer IT Consulting.
+ * Released under an MIT License.
+ */
+ 
+#include <emdros/emdfdb.h>
+#include <emdros/emdros_environment.h>
+#include <emdros/mql_sheaf.h>
+#include <emdros/emdf_value.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "csv-parser-master/csv.hh"
+
+#include "emdros_iterators.hpp"
+
+using namespace std;
+
+enum class cols {
+    original_order,book,chapter,verse,lex,verb_classes,verb_classes_correction,
+    g_word_nocant,not_usable_for_exercises_because_kq,g_word_nocant_utf8,
+    ps1,nu1,gn1,vt1,vs1,suffix_person1,suffix_number1,suffix_gender1,
+    ps2,nu2,gn2,vt2,vs2,suffix_person2,suffix_number2,suffix_gender2,
+    ps3,nu3,gn3,vt3,vs3,suffix_person3,suffix_number3,suffix_gender3,
+    ps4,nu4,gn4,vt4,vs4,suffix_person4,suffix_number4,suffix_gender4
+};
+
+string at(const vector<string>& r, int c)
+{
+    if (c >= r.size())
+        return "unknown";
+    else
+        return r.at(c);
+}
+
+string at(const vector<string>& r, cols c)
+{
+    return at(r,(int)c);
+}
+
+int count_var(const vector<string>& r)
+{
+    int count = 0;
+
+    if (!at(r,cols::ps1).empty()) ++count;
+    if (!at(r,cols::ps2).empty()) ++count;
+    if (!at(r,cols::ps3).empty()) ++count;
+    if (!at(r,cols::ps4).empty()) ++count;
+
+    return count;
+}
+
+
+class selector {
+  public:
+    static string diff2(const vector<string>&r) {
+        for (int i=0; i<feat_diff2.size(); ++i) {
+            cols col1 = feat_diff1[i];
+            cols col2 = feat_diff2[i];
+
+            if (alldiff(at(r,col1), at(r,col2)))
+                return feat_diff2string.at(i) + "=" + at(r,col1);
+        }
+        cerr << "INDETERMINATE\n";
+    }
+
+    static string diff3(const vector<string>&r) {
+        for (int i=0; i<feat_diff1.size(); ++i) {
+            cols col1 = feat_diff1[i];
+            cols col2 = feat_diff2[i];
+            cols col3 = feat_diff3[i];
+            
+            if (alldiff(at(r,col1), at(r,col2), at(r,col3)))
+                return feat_diff2string.at(i) + "=" + at(r,col1);
+        }
+        cerr << "INDETERMINATE\n";
+    }
+
+    static string diff4(const vector<string>&r) {
+        for (int i=0; i<feat_diff1.size(); ++i) {
+            cols col1 = feat_diff1[i];
+            cols col2 = feat_diff2[i];
+            cols col3 = feat_diff3[i];
+            cols col4 = feat_diff4[i];
+            
+            if (alldiff(at(r,col1), at(r,col2), at(r,col3), at(r,col4)))
+                return feat_diff2string.at(i) + "=" + at(r,col1);
+        }
+
+        // Indeterminate based on one feature, let's try two features
+        for (int i=0; i<feat_diff1.size(); ++i) {
+            for (int j=i+1; j<feat_diff1.size(); ++j) {
+            
+                string s1a = at(r, feat_diff1[i]);
+                string s2a = at(r, feat_diff2[i]);
+                string s3a = at(r, feat_diff3[i]);
+                string s4a = at(r, feat_diff4[i]);
+                string s1b = at(r, feat_diff1[j]);
+                string s2b = at(r, feat_diff2[j]);
+                string s3b = at(r, feat_diff3[j]);
+                string s4b = at(r, feat_diff4[j]);
+
+                if (s1a=="unknown" || s1b=="unknown" ||
+                    s2a=="unknown" || s2b=="unknown" ||
+                    s3a=="unknown" || s3b=="unknown" ||
+                    s4a=="unknown" || s4b=="unknown")
+                    continue;
+                
+                string s1 = s1a + "." + s1b;
+                string s2 = s2a + "." + s2b;
+                string s3 = s3a + "." + s3b;
+                string s4 = s4a + "." + s4b;
+
+                if (alldiff(s1,s2,s3,s4))
+                    return feat_diff2string.at(i) + "=" + s1a + ","
+                        + feat_diff2string.at(j) + "=" + s1b;
+            }
+        }
+
+        // Indeterminate based on two features, let's allow "unknown" values
+        for (int i=0; i<feat_diff1.size(); ++i) {
+            for (int j=i+1; j<feat_diff1.size(); ++j) {
+            
+                string s1a = at(r, feat_diff1[i]);
+                string s2a = at(r, feat_diff2[i]);
+                string s3a = at(r, feat_diff3[i]);
+                string s4a = at(r, feat_diff4[i]);
+                string s1b = at(r, feat_diff1[j]);
+                string s2b = at(r, feat_diff2[j]);
+                string s3b = at(r, feat_diff3[j]);
+                string s4b = at(r, feat_diff4[j]);
+
+                string s1 = s1a + "." + s1b;
+                string s2 = s2a + "." + s2b;
+                string s3 = s3a + "." + s3b;
+                string s4 = s4a + "." + s4b;
+
+                if (alldiff(s1,s2,s3,s4))
+                    return feat_diff2string.at(i) + "=" + s1a + ","
+                        + feat_diff2string.at(j) + "=" + s1b;
+            }
+        }
+
+        cerr << "INDETERMINATE\n";
+    }
+
+  private:
+    // This identifies the feature differentiators by order of relevance.
+    inline static array feat_diff1{cols::ps1, cols::gn1, cols::nu1, cols::vt1, cols::vs1, cols::suffix_person1, cols::suffix_number1, cols::suffix_gender1};
+    inline static array feat_diff2{cols::ps2, cols::gn2, cols::nu2, cols::vt2, cols::vs2, cols::suffix_person2, cols::suffix_number2, cols::suffix_gender2};
+    inline static array feat_diff3{cols::ps3, cols::gn3, cols::nu3, cols::vt3, cols::vs3, cols::suffix_person3, cols::suffix_number3, cols::suffix_gender3};
+    inline static array feat_diff4{cols::ps4, cols::gn4, cols::nu4, cols::vt4, cols::vs4, cols::suffix_person4, cols::suffix_number4, cols::suffix_gender4};
+
+    // Translates int(feat_diff) to string
+    inline static array feat_diff2string{"ps"s, "nu"s, "gn"s, "vt"s, "vs"s, "suffix_person"s, "suffix_number"s, "suffix_gender"s};
+
+    static bool alldiff(const string& s1, const string& s2) {
+        return s1!=s2 && s1!="unknown" && s2!="unknown";
+    }
+
+    static bool alldiff(const string& s1, const string& s2, const string& s3) {
+        return s1!=s2 && s1!=s3 && s2!=s3
+            && s1!="unknown" && s2!="unknown" && s3!="unknown";
+    }
+
+    static bool alldiff(const string& s1, const string& s2, const string& s3, const string& s4) {
+        return s1!=s2 && s1!=s3 && s1!=s4 && s2!=s3 && s2!=s4 && s3!=s4
+            && s1!="unknown" && s2!="unknown" && s3!="unknown" && s4!="uknown";
+    }
+};
+ 
+
+
+int main(int argc, char **argv)
+{
+    if (argc!=3) {
+        cerr << "usage: hintsdb <emdrosfile> <sqlfile>" << endl;
+        return 1;
+    }
+
+    read_csv_t csv;
+    string csvfile{"BibleOL_verbal-ambiguity-project_v1.23.csv"};
+    
+    if (csv.open(csvfile) < 0) {
+        cout << "Cannot open file " << csvfile << "\n";
+        return 1;
+    }
+    csv.read_row(); // Read header
+
+    ofstream sqlfile{argv[2]};
+    sqlfile << "CREATE TABLE hints (self integer primary key, hint text);\n";
+    sqlfile << "BEGIN TRANSACTION;\n";
+
+    
+    EmdrosEnv EE{kOKConsole,
+            kCSUTF8,
+            "localhost",
+            "",
+            "",
+            argv[1],
+            kSQLite3};
+
+    bool bResult{false};
+
+    string mql_request{"SELECT ALL OBJECTS WHERE [word sp=verb AND language=Hebrew GET self,g_word_nocant,"
+            "ps,nu,gn,vt,vs,suffix_person,suffix_number,suffix_gender] GO"};
+    
+    if (!EE.executeString(mql_request, bResult, false, true))
+        return 1;
+
+    if (!EE.isSheaf()) {
+        cerr << "ERROR: Result is not sheaf\n";
+        return 1;
+    }
+
+
+    map<int, map<string, string>> feature_maps; // Maps id_d to feature=>value map
+
+    for (StrawOk str : SheafOk{EE.getSheaf()}) {
+        long self = 0;
+        string g_word_nocant;
+        string features[8]; //ps,nu,gn,vt,vs,suffix_person,suffix_number,suffix_gender
+        
+        for (const MatchedObject mo : str) {
+            self = mo.getFeatureAsLong(0);
+            g_word_nocant = mo.getFeatureAsString(1);
+            for (int i=0; i<8; ++i) {
+                features[i] = mo.getFeatureAsString(2+i);
+                if (i>=5 && features[i]=="absent")
+                    features[i]="unknown";
+            }
+        }
+
+        vector<string> row = csv.read_row();
+        if (row.size()==0) {
+            cerr << "Unexpected OEF in spreadsheet\n";
+            return 1;
+        }
+        
+        if (g_word_nocant != at(row,cols::g_word_nocant)) {
+            cerr << "Inconsistency between database and spreadsheet at original order " << at(row,cols::original_order) << '\n'
+                 << "Spreadsheet has " << at(row,cols::g_word_nocant)
+                 << " Emdros has " << g_word_nocant << '\n';
+            return 1;
+        }
+
+        for (int i=0; i<8; ++i) {
+            if (features[i] != at(row,int(cols::ps1) + i)) {
+                cerr << "Inconsistency between database and spreadsheet at original order " << at(row,cols::original_order) << " word is " << g_word_nocant << '\n'
+                     << "Spreadsheet has feature " << i << "=" << at(row,int(cols::ps1)+i)
+                     << " Emdros has " << features[i] << '\n';
+//                return 1;
+            }
+        }
+
+
+        switch (count_var(row)) {
+          case 4:
+                sqlfile << "INSERT INTO hints VALUES(" << self << ",'" << selector::diff4(row) << "');\n";
+                break;
+          case 3:
+                sqlfile << "INSERT INTO hints VALUES(" << self << ",'" << selector::diff3(row) << "');\n";
+                break;
+          case 2:
+                sqlfile << "INSERT INTO hints VALUES(" << self << ",'" << selector::diff2(row) << "');\n";
+                break;
+        }
+    }
+    
+    sqlfile << "COMMIT;\n";
+}
