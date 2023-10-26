7b2744c06975655f313612e7dfa86efecc701f45
Claus Tøndering <claus@tondering.dk>
2023-10-26 10:24:38 +0200

    New spreadsheet without holes in alternatives

--- hintsdb.cpp
+++ hintsdb.cpp
@@ -289,7 +289,7 @@ int main(int argc, char **argv)
         int csv_row = 0;
         
         string csvfile{lang=="Hebrew"
-            ? "HEBREW_BibleOL_verbal-ambiguity-project_v1.50-heb.csv"
+            ? "HEBREW_BibleOL_verbal-ambiguity-project_v1.51-heb.csv"
             : "BibleOL_verbal-ambiguity-project_v1.48-aram.csv"};
 
         try {
